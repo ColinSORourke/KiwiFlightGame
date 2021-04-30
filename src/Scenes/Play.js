ce49d656c8e7ef4ab45a1ac1b1a15d2b9ab69389
ColinSORourke <csorourk@ucsc.edu>
2021-04-29 17:34:34 -0700

    Stretch Snapping + Loaded*Animed Sprites

--- src/Scenes/Play.js
+++ src/Scenes/Play.js
@@ -6,13 +6,16 @@ class Play extends Phaser.Scene {
     preload() {
       this.load.image("ScrollBG", "./assets/TempBackground.jpg");
       this.load.image("Ground", "./assets/Ground.png");
-      this.load.spritesheet("Kiwi", "./assets/KiwiRunFullSpritesheet.png", {frameWidth: 938, frameHeight: 847});
+      this.load.atlas('kiwi', './assets/BodyAndHeadAnims.png', './assets/BodyAndHeadAnims.json');
       this.load.image('square', "./assets/BrownSquare.png");
       this.load.image('squareB', "./assets/BrownSquareB.png");
     }
     
     create() {
         this.matter.world.setBounds(0, 0, game.config.width, game.config.height);
+
+        var kiwiTexture = this.textures.get('kiwi');
+        console.log(kiwiTexture);
 
         var defaultCategory = 0x0001,
             kiwiCategory = 0x0002;
@@ -24,8 +27,8 @@ class Play extends Phaser.Scene {
         var ground = this.matter.add.image(game.config.width/2, game.config.height - screenUnit/2, 'Ground', null, {isStatic: true });
 
         // Add Body
-        this.body = this.matter.add.image(200, game.config.height - 120, 'square', null, {
-          label: 'kiwiBody',
+        this.body = this.matter.add.sprite( 200, game.config.height - 120, "kiwi", "tile000.png", {
+          label: 'Kiwi',
           collisionFilter: {
             category: kiwiCategory,
             mask: defaultCategory
@@ -33,13 +36,33 @@ class Play extends Phaser.Scene {
         });
 
         // Add Head
-        this.head = this.matter.add.image(200, game.config.height - 200, 'squareB', null, {
-          label: 'kiwiHead',
+        this.head = this.matter.add.sprite( 200, game.config.height - 200, "kiwi", "head000.png", {
+          label: 'Kiwi',
           collisionFilter: {
             category: kiwiCategory,
             mask: defaultCategory
           }
         });
+
+        var headFrameNames = this.anims.generateFrameNames('kiwi', { prefix: 'head', suffix: ".png", end: 29, zeroPad: 3 });
+        var bodyFrameNames = this.anims.generateFrameNames('kiwi', { prefix: 'tile', suffix: ".png", end: 29, zeroPad: 3 });
+
+        this.anims.create({
+          key: 'bodyRun',
+          frames: bodyFrameNames,
+          frameRate: 30,
+          repeat: -1
+        });
+        this.body.anims.play('bodyRun', true);
+
+        var frameNames = this.anims.generateFrameNames('KiwiHead')
+        this.anims.create({
+          key: 'headRun',
+          frames: headFrameNames,
+          frameRate: 30,
+          repeat: -1
+        });
+        this.head.anims.play('headRun', true);
 
         // Add constraint that represents neck
         this.neckConst = this.matter.add.constraint(this.body, this.head, 70, 1, {angularStiffness: 100});
@@ -57,16 +80,29 @@ class Play extends Phaser.Scene {
       // Scroll Background
       this.bg.tilePositionX += 8;
 
-      if (keySPACE.isDown && !this.vaulting && this.neckConst.length <= 600) {
+      if (keySPACE.isDown && !this.vaulting && this.neckConst.length <= 550) {
         // Stretch neck constraint. When Head is perfectly balanced straight above body, this sends head straight up
-        this.neckConst.length += 5;
+        this.neckConst.length += 7;
       }
 
       if (Phaser.Input.Keyboard.JustUp(keySPACE) && !this.vaulting){
         if (this.neckConst.length >= 170){
           // Do vault
-          this.vaulting = true;
-          this.doVault();
+          let vault = this.vaulting;
+          vault = true;
+          let myHead = this.head.setStatic(true);
+          this.body.setStatic(false);
+          this.tweens.add({
+            targets: this.neckConst,
+            length: 70,
+            duration: 400,
+            ease: 'Linear',
+            onComplete: function(){
+              myHead.setStatic(false);
+              vault = false;
+            }
+          });
+          
         } else {
           // Retract neck
           this.tweens.add({
@@ -77,47 +113,5 @@ class Play extends Phaser.Scene {
           });
         }
       }
-    }
-
-    doVault(){
-      let myHead = this.head;
-      let myBod = this.body;
-      let scene = this;
-
-
-      // Head falls, rotating around fixed point body.
-      myBod.setStatic(true);
-      myHead.setStatic(false);
-      myHead.applyForce({x: 0.25, y: 0});
-      
-
-      // When the head hits the ground
-      myHead.setOnCollide(function(event) {
-        // We need to apply force using this method, Matter.js apparntly clears all forces on event emit
-        scene.matter.world.once('beforeupdate', function(){
-
-          // Body launches, rotating around fixed point head
-          myBod.setStatic(false);
-          myHead.setStatic(true);
-          myBod.applyForce({x: 0, y: -1});
-
-          // Retract neck length
-          scene.tweens.add({
-            targets: scene.neckConst,
-            length: 70,
-            duration: 1000,
-            ease: 'Linear',
-          });
-
-          // Remove both collide functions so they don't re-trigger
-          myBod.setOnCollide(function(event) {
-            myBod.setOnCollide(function() { /* do nothing */ });
-            myHead.setOnCollide(function() { /* do nothing */ });
-          });
-
-          // This no longer vaulting.
-          scene.vaulting = false;
-        });
-      })
     }
   }
